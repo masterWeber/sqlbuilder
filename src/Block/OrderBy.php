c57eda3b9a1b1a30ae006088d958de1b906d942a
masterWeber <master.weber@outlook.com>
2021-01-04 11:52:03 +0300

    Added Block\OrderBy

---
+++ src/Block/OrderBy.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace Block;
+
+
+use Clause\LimitClause;
+
+class OrderBy
+{
+  const STATEMENT = 'ORDER BY';
+  const ASC = 'ASC';
+  const DESC = 'DESC';
+
+  /**
+   * @var mixed
+   */
+  protected $parent;
+  /**
+   * [
+   *   ['column_name', 'ASC'],
+   *   ['column_name_1', 'DESC']
+   * ]
+   */
+  protected array $expressionList;
+
+  use LimitClause;
+
+  public function __construct($expression, $sorting = self::ASC, $parent = null)
+  {
+    if (is_array($expression)) {
+
+      foreach ($expression as $key => $value) {
+        if (is_string($key)) {
+          $this->expressionList[] = [$key, $value];
+        } else {
+          $this->expressionList[] = [$value, self::ASC];
+        }
+      }
+
+    } else {
+      $this->expressionList[] = [$expression, $sorting];
+    }
+
+    $this->parent = $parent;
+  }
+
+  public function __toString(): string
+  {
+    if (empty($this->expressionList)) {
+      return '';
+    }
+
+    $str = '';
+    foreach ($this->expressionList as $orderBy) {
+      $str .= ', ' . $orderBy[0];
+      if (array_key_exists(1, $orderBy)) {
+        $str .= ' ' . $orderBy[1];
+      }
+    }
+
+    $str = trim(ltrim($str, ','));
+
+    return trim($this->parent . ' ' . self::STATEMENT . ' ' . $str);
+  }
+}
---
+++ tests/Block/OrderByTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Block;
+
+use PHPUnit\Framework\TestCase;
+
+class OrderByTest extends TestCase
+{
+
+  public function testToString()
+  {
+    $orderBy = new OrderBy('col_name', OrderBy::DESC);
+
+    $this->assertEquals(
+      'ORDER BY col_name DESC',
+      $orderBy->__toString()
+    );
+
+    $orderBy = new OrderBy([
+      'col_name' => OrderBy::DESC,
+      'col_name_1' => OrderBy::ASC,
+    ]);
+
+    $this->assertEquals(
+      'ORDER BY col_name DESC, col_name_1 ASC',
+      $orderBy->__toString()
+    );
+  }
+}
