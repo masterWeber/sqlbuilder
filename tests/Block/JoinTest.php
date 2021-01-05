5d6e965dc2dd0872e1089256e7f0b512063631fc
masterWeber <master.weber@outlook.com>
2021-01-05 11:47:31 +0300

    Added JoinTest

--- tests/Block/JoinTest.php
+++ tests/Block/JoinTest.php
@@ -9,10 +9,20 @@ class JoinTest extends TestCase
   public function testToString()
   {
     $join = new Join(['table_name' => 'alias']);
-    $sql = $join->on()->equal('t1.col', 't2.col');
+    $sql = $join->on()
+      ->equal('t1.col', 't2.col');
 
     $this->assertEquals(
-      '',
+      "JOIN table_name AS alias ON (t1.col = 't2.col')",
+      $sql->__toString()
+    );
+
+    $join = new Join(['table_name' => 'alias']);
+    $sql = $join->left()->on()
+      ->equal('t1.col', 't2.col');
+
+    $this->assertEquals(
+      "LEFT JOIN table_name AS alias ON (t1.col = 't2.col')",
       $sql->__toString()
     );
   }
