bafedece984ad4f2ee7f07f202b7fc56db82c3ca
masterWeber <master.weber@outlook.com>
2021-01-08 19:31:00 +0300

    Added Option

---
+++ src/Option.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace SQLBuilder;
+
+
+class Option
+{
+  const IF_NOT_EXISTS = 'IF NOT EXISTS';
+  const IF_EXISTS = 'IF EXISTS';
+  const CHARACTER_SET = 'CHARACTER SET';
+  const COLLATE = 'COLLATE';
+  const ENCRYPTION = 'ENCRYPTION';
+}
